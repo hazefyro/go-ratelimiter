de31c0fccac869260a03099fe062ac9cbac2cb60
haze <92948735+hazetwo@users.noreply.github.com>
2026-05-25 11:14:12 +0200

    ratelimiter tests

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -3,6 +3,7 @@ package ratelimiter_test
 import (
 	"errors"
 	"testing"
+	"time"
 
 	ratelimiter "github.com/haze/go-ratelimiter"
 )
@@ -25,7 +26,7 @@ func TestAllow_permits_under_limit(t *testing.T) {
 		Bucket:    10,
 	})
 
-	if err := rl.Allow("192.0.2.1"); err != nil {
+	if err := rl.Allow(key); err != nil {
 		t.Fatalf("expected nil, got %v", err)
 	}
 }
@@ -36,9 +37,7 @@ func TestAllow_returns_ErrRateLimited(t *testing.T) {
 		Bucket:    1,
 	})
 
-	if err := rl.Allow(key); err != nil {
-		t.Fatalf("expected nil, got %v", err)
-	}
+	rl.Allow(key)
 
 	if err := rl.Allow(key); !errors.Is(err, ratelimiter.ErrRateLimited) {
 		t.Fatalf("expected ErrRateLimited, got %v", err)
@@ -69,12 +68,96 @@ func TestAllowN_returns_ErrRateLimited_when_insufficient_tokens(t *testing.T) {
 	}
 }
 
-// func TestAllow_bans_after_threshold(t *testing.T){
-//     rl := newLimiter(t, &ratelimiter.Options{
-// 		RateLimit: 10,
-// 		Bucket:    5,
-// 		ViolationThreshold: 1,
-// 		ViolationWindow: 1,
-// 		BanDuration: 5,
-// 	})
-// }
+func TestAllow_bans_after_threshold(t *testing.T) {
+	rl := newLimiter(t, &ratelimiter.Options{
+		RateLimit: 1,
+		Bucket:    1,
+		Banning: &ratelimiter.BanOptions{
+			Duration:  time.Second + 30*time.Millisecond,
+			Window:    time.Second,
+			Threshold: 1,
+		},
+	})
+
+	for range 2 {
+		rl.Allow(key)
+	}
+
+	if err := rl.Allow(key); !errors.Is(err, ratelimiter.ErrBanned) {
+		t.Fatalf("expected ErrBanned, got %v", err)
+	}
+
+}
+
+func TestAllow_returns_ErrBanned_while_banned(t *testing.T) {
+	rl := newLimiter(t, &ratelimiter.Options{
+		RateLimit: 1,
+		Bucket:    1,
+		Banning: &ratelimiter.BanOptions{
+			Duration:  time.Second + 30*time.Millisecond,
+			Window:    time.Second,
+			Threshold: 1,
+		},
+	})
+
+	for range 2 {
+		rl.Allow(key)
+	}
+
+	for range 5 {
+		if err := rl.Allow(key); !errors.Is(err, ratelimiter.ErrBanned) {
+			t.Fatalf("expected ErrBanned, got %v", err)
+		}
+	}
+
+}
+
+func TestAllow_resets_violations_after_window(t *testing.T) {
+	rl := newLimiter(t, &ratelimiter.Options{
+		RateLimit: 1,
+		Bucket:    1,
+		Banning: &ratelimiter.BanOptions{
+			Duration:  time.Second + 30*time.Millisecond,
+			Window:    time.Second,
+			Threshold: 2,
+		},
+	})
+
+	for range 2 {
+		rl.Allow(key)
+	}
+
+	time.Sleep(time.Second)
+
+	if err := rl.Allow(key); err != nil {
+		t.Fatalf("expected nil, got %v", err)
+	}
+
+}
+
+func TestAllow_lifts_ban_after_durationfunc(t *testing.T) {
+	rl := newLimiter(t, &ratelimiter.Options{
+		RateLimit: 1,
+		Bucket:    1,
+		Banning: &ratelimiter.BanOptions{
+			Duration:  time.Second + 30*time.Millisecond,
+			Window:    time.Second,
+			Threshold: 1,
+		},
+	})
+
+	for range 2 {
+		rl.Allow(key)
+	}
+
+	if err := rl.Allow(key); !errors.Is(err, ratelimiter.ErrBanned) {
+		t.Fatalf("expected ErrBanned, got %v", err)
+	}
+
+	time.Sleep(time.Second + 30*time.Millisecond)
+
+	if err := rl.Allow(key); err != nil {
+		t.Fatalf("expected nil, got %v", err)
+	}
+
+}
